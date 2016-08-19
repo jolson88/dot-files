b2ef66431a054e7cfe5948c312e479a91f794d9a
Jason Olson <jason.olson@concur.com>
2016-08-18 22:57:27 -0700

    Symlink dotfiles

--- setup-tools.sh
+++ setup-tools.sh
@@ -3,7 +3,7 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mkdir -p ~/.antigen/
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/.antigen/antigen.zsh
-cp .zshrc ~/.zshrc
+ln -s /Users/jolson/source/personal/dev-tools/.zshrc /Users/jolson/.zshrc
 
 # iTERM2
 brew cask install iterm2
@@ -15,13 +15,16 @@ rm -rf ~/.emacs
 brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
 brew linkapps
-cp .spacemacs ~/.spacemacs
+ln -s /Users/jolson/source/personal/dev-tools/.spacemacs /Users/jolson/.spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 # NEOVIM
 brew tap neovim/neovim
 brew install --HEAD neovim
 cp .nvimrc ~/.nvimrc
+
+# SYMLINKS
+ln -s /Users/jolson/source/personal/dev-tools/.nvimrc /Users/jolson/.nvimrc
 
 # DOCKER
 echo ""
