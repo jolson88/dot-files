44aa6cb691f6613da9e4f4ec892ab47b016fd63b
Jason Olson <jolson88@outlook.com>
2019-08-26 11:42:46 -0700

    Initializes ssh-agent in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,26 +11,7 @@ abbr -a gri 'git rebase -i'
 abbr -a m make
 
 set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
-
-# Helper functions
-function __ssh_agent_start -d "start a new ssh agent"
-  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-  chmod 600 $SSH_ENV
-  source $SSH_ENV > /dev/null
-end
-
-function __ssh_agent_is_started -d "check if ssh agent is already started"
-  	if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
-		source $SSH_ENV > /dev/null
-	end
-
-	if test -z "$SSH_AGENT_PID"
-		return 1
-	end
-
-	ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
-	return $status
-end
+eval (keychain --eval id_rsa)
 
 function d
 	while test $PWD != "/"
@@ -39,15 +20,6 @@ function d
 		end
 		cd ..
 	end
-end
-
-# Start SSH
-if test -z "$SSH_ENV"
-    set -xg SSH_ENV $HOME/.ssh/environment
-end
-
-if not __ssh_agent_is_started
-    __ssh_agent_start
 end
 
 # Fish git prompt
