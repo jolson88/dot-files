1c1318653cc258367cdaa6bf16915d321610cf4f
Jason Olson <jolson88@outlook.com>
2019-08-27 17:30:54 -0700

    More setup script

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,28 +1,48 @@
+#################
+# Initial setup
+#################
 # If on XPS 9570, run the xps tweaks command:
 # sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/JackHack96/dell-xps-9570-ubuntu-respin/master/xps-tweaks.sh)"
 # Import id_rsa and id_rsa.pub
 
+#######################
 # Install core tools
+#######################
 sudo apt install keyring
 sudo apt install git
 sudo apt install build-essential
 sudo apt install tmux
+
+####################
+# Shells/Terminals
+####################
 sudo apt install rxvt-unicode xsel
 sudo apt install fish
-
-# Change shells/terminals
 sudo chsh -s /usr/bin/fish
 sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
 
+##################################
 # Set permissions and import keys
-chmod 400 ~/.ssh/id_rsa
+##################################
 # Import a public GPG key: `gpg --import public.key
 # Import a private GPG key: `gpg --allow-secret-key-import --import private.key
 # Use `gpg --list-keys` to find GPG key
 # `git config --global user.signingkey FOO` to sign
 
+############################
 # Install dev tools
+############################
 
+# Rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+rustup update
+rustup component add rustfmt
+rustup component add rls rust-analysis rust-src
 
+###################
 # Manual steps
+###################
+echo "Don't forget to import ssh keys and GPG keys"
+# chmod 400 ~/.ssh/id_rsa
 echo "Now install config files from dot-files and reboot"
